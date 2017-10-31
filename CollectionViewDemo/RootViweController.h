5e24d5f9b44c9235ee61d0d14dd2ae096d8d25f2
hanserHe <94189786@qq.com>
2017-10-31 10:41:11 +0800

    first commit

---
+++ CollectionViewDemo/RootViweController.h
@@ -0,0 +1,13 @@
+//
+//  RootViweController.h
+//  CollectionViewDemo
+//
+//  Created by Hanser on 31/10/2017.
+//  Copyright © 2017 Mr.H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViweController : UIViewController
+
+@end
---
+++ CollectionViewDemo/RootViweController.m
@@ -0,0 +1,28 @@
+//
+//  RootViweController.m
+//  CollectionViewDemo
+//
+//  Created by Hanser on 31/10/2017.
+//  Copyright © 2017 Mr.H. All rights reserved.
+//
+
+#import "RootViweController.h"
+
+@interface RootViweController ()
+
+@end
+
+@implementation RootViweController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.navigationItem.title = @"CollectionView";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
